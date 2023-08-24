01c81db78da2ea83c8c10c2bedcc0692dedc3bbc
Mohammad Mehdi Beheshti <shadymx@yahoo.com>
2023-08-24 21:07:23 +0330

    Adding more colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,15 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
 
+	darkMode: "class",
+
 	theme: {
 		colors: {
 			black: "#121212",
 			darkPurple: "#322653",
 			darkGray: "#474747",
+			blackPurple: "#282A3A",
+			blackGray: "#2C3333",
 			pink: "#dd58d6",
 			lightPurple: "#893cdc",
 			white: "#fefefe",
@@ -47,8 +51,17 @@ export default {
 				}
 				return spacing;
 			})(),
-		},
-	},
 
-	plugins: [],
+			minWidth: (() => {
+				const minWidth = {};
+				for (let i = 1; i <= 1000; i++) {
+					const rem = parseFloat(i * 0.1);
+					minWidth[rem] = `${rem}rem`;
+				}
+				return minWidth;
+			})(),
+		},
+
+		plugins: [],
+	},
 };
