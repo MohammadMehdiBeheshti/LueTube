a9660c2bedd5530fd3c912c8d2c429e520a11d98
Mohammad Mehdi Beheshti <shadymx@yahoo.com>
2023-08-26 13:42:35 +0330

    Updating mobile device detector

--- src/utilities/isMobile.js
+++ src/utilities/isMobile.js
@@ -1,7 +1,8 @@
 const isMobile = () => {
 	const maxTouchPoints = navigator.maxTouchPoints > 0;
 	const userAgent = /Android|iPhone/i.test(navigator.userAgent);
-	return maxTouchPoints || userAgent;
+	const smallBreakPoint = innerWidth <= 576;
+	return maxTouchPoints || userAgent || smallBreakPoint;
 };
 
 export default isMobile;
