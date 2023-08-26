4506f34442e6e1f2c922d89ad224caf67f78df51
Mohammad Mehdi Beheshti <shadymx@yahoo.com>
2023-08-26 13:42:16 +0330

    Fixing footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,9 +2,11 @@ import { Logo } from "./Icons.jsx";
 
 export default function Footer({ cards }) {
 	return cards.length ? (
-		<footer className="contain h-7 mt-8 flex justify-between items-center rounded-def bg-lightPurple dark:bg-blackGray">
-			<span className="text-md font-fontSB">All rights reserved :(</span>
-			<Logo w={150} />
+		<footer className="h-7 flex items-center mt-8 bg-lightPurple dark:bg-blackGray">
+			<div className="contain flex justify-between items-center">
+				<span className="text-md font-fontSB">All rights reserved :(</span>
+				<Logo className="w-15" />
+			</div>
 		</footer>
 	) : null;
 }
